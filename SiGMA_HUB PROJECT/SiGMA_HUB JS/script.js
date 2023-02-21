e6bdaa760d90940cfcf05d254db0f540ee338fda
aj05hacker <84639525+aj05hacker@users.noreply.github.com>
2023-02-21 23:01:54 +0530

    Update script.js

--- SiGMA_HUB PROJECT/SiGMA_HUB JS/script.js
+++ SiGMA_HUB PROJECT/SiGMA_HUB JS/script.js
@@ -1,3 +1,7 @@
+//REDIRECT PAGE (START)
+
 function redirectToPage() {
     window.location.href = "../SiGMA_HUB HTML/discoverpage.html"; // Replace with the URL you want to redirect to
   }
+
+//REDIRECT PAGE (END)
